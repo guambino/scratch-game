d65da1c752e9146676bffc3ad80b333553676e86
guambino <ivan.guambe@quankee.co.za>
2025-07-01 17:55:41 +0200

    Menu is ready

--- src/main/java/ae/cyberspeed/scratch/game/service/MenuService.java
+++ src/main/java/ae/cyberspeed/scratch/game/service/MenuService.java
@@ -1,10 +1,14 @@
 package ae.cyberspeed.scratch.game.service;
 
+import java.util.Scanner;
+
 public class MenuService {
 
     private final String configJsonFile;
 
     private final Integer bettingAmount;
+
+    private static final String ASTERICK_LINE = "********************************************************";
 
     public MenuService(String configJsonFile, Integer bettingAmount) {
         this.configJsonFile = configJsonFile;
@@ -12,16 +16,40 @@ public class MenuService {
     }
 
     public void runMenu(){
+        menuIntro();
+        buildMenu();
+    }
 
-        System.out.println("******************************************************** ");
-        System.out.println("           Welcome To Scratch Game ");
-        System.out.println("******************************************************** ");
+    private void menuIntro(){
+        System.out.println(ASTERICK_LINE);
+        System.out.println("           Welcome To Scratch Game                       ");
+        System.out.println(ASTERICK_LINE);
         System.out.println();
         System.out.println();
         System.out.println("Config File: " + configJsonFile);
-        System.out.println("Betting Amount " + bettingAmount);
+        System.out.println("Betting Amount: " + bettingAmount);
         System.out.println();
         System.out.println();
+    }
 
+    private void buildMenu(){
+        Scanner choice = new Scanner(System.in);
+
+        String menuChoice = "";
+
+        while (!menuChoice.equalsIgnoreCase("0")){
+            System.out.println(ASTERICK_LINE);
+            System.out.println("                     Menu                                ");
+            System.out.println(ASTERICK_LINE);
+            System.out.println(" 1. Run Game ");
+            System.out.println(" 0. Exit ");
+            menuChoice = choice.nextLine();
+
+            //Options
+            switch (menuChoice){
+                case "1": System.out.println(" You about to run the game. "); break;
+                case "0": System.exit(0); break;
+            }
+        }
     }
 }
